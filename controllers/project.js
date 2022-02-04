9432e07a0865fea87d926207ec8158747c0cc4bb
luiisfabian <luiisfabian96@gmail.com>
2022-02-04 18:12:29 -0500

    get projects, update by id, delete by id

--- controllers/project.js
+++ controllers/project.js
@@ -41,17 +41,58 @@ let controllers = {
 
     },
 
-    getPorject: function(req, res){
+    getPorjectId: function (req, res) {
         let projectId = req.params.id;
 
         if (projectId == null) {
-            return res.status(404).send({message: "el documento no existe"});   
+            return res.status(404).send({ message: "el documento no existe" });
         }
 
-        project.findById(projectId, (err, project)=>{
-            if(err) return res.status(500).send({message: "error al devolver los datos"});
-            if(!project) return res.status(404).send({message: "el documento no existe"});
-            return res.status(200).send({project: project})
+        project.findById(projectId, (err, project) => {
+            if (err) return res.status(500).send({ message: "error al devolver los datos" });
+            if (!project) return res.status(404).send({ message: "el documento no existe" });
+            return res.status(200).send({ project: project })
+        })
+    },
+
+    getProjects: function (req, res) {
+
+        project.find({ isDeleted: false }).sort('+year').exec((err, projects) => {
+
+
+            if (err) return res.status(500).send({ message: "error al devolver los datos" });
+            if (!project) return res.status(404).send({ message: "no hay datos para mostrar" });
+            return res.status(200).send(projects);
+        })
+
+    },
+
+    updateProjectbyId: function (req, res) {
+
+        let projectId = req.params.id;
+        let update = req.body;
+
+        project.findByIdAndUpdate(projectId, update, { new: true }, (err, projectUpdated) => {
+            if (err) return req.status(500).send({ message: "error al actualizar" });
+            if (!projectUpdated) return rq.status(404).send({ message: "no se ha podudi actualizar" });
+            return res.status(200).send({
+                message: "Actualizo melo",
+                project: projectUpdated
+            })
+        });
+    },
+
+    deleteProject: function(req, res){
+
+        let projectId = req.params.id
+
+        project.findByIdAndRemove(projectId, (err, projectDeleted)=>{
+            if (err) return req.status(500).send({ message: "error al eliminar" });
+            if (!projectDeleted) return rq.status(404).send({ message: "no se ha podido Eliminar" });
+            return res.status(200).send({
+                message: "Eliminado con exito",
+                project: projectDeleted
+            })
         })
     }
 
--- rutes/project.js
+++ rutes/project.js
@@ -3,14 +3,17 @@
 
 
 let express = require('express');
+const { getProjects } = require('../controllers/project');
 let projectController = require('../controllers/project');
 let router = express.Router();
 
 router.get('/home', projectController.home);
 router.post('/test', projectController.test);
 router.post('/saveProject', projectController.saveProject);
-router.get('/project/:id?', projectController.getPorject);
-
+router.get('/project/:id?', projectController.getPorjectId);
+router.get('/projects', projectController.getProjects);
+router.put('/project/:id', projectController.updateProjectbyId);
+router.delete('/project/:id', projectController.deleteProject);
 
 
 
